35e38aac4daa8347e1363ba222f637e72222a896
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-11-18 10:12:13 +0600

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Эльвира, <br />хорош дуться!<span role="img" aria-label="sheep">🥺🤍</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
